c41fc3732b206cbd6ea3d597dbb291f795a3aa18
Sirio <sirioroberto1@hotmail.com>
2023-09-17 20:07:16 -0300

    Improved controller to create new recipes and get them by id. Also implemented exception when id does not exist

--- src/recipes/presentation/RecipeController.java
+++ src/recipes/presentation/RecipeController.java
@@ -1,23 +1,33 @@
 package recipes.presentation;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import recipes.business.Recipe;
+import recipes.business.RecipeDto;
+import recipes.business.RecipeService;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/recipe")
 public class RecipeController {
-    private Recipe recipe = new Recipe();
+    private final RecipeService service;
 
-    @GetMapping("recipe")
-    public ResponseEntity<Object> getRecipe() {
-        return ResponseEntity.ok(recipe);
+    public RecipeController(RecipeService service) {
+        this.service = service;
     }
 
-    @PostMapping("recipe")
-    public ResponseEntity<Object> postRecipe(@RequestBody Recipe recipe) {
-        this.recipe = recipe;
-        return ResponseEntity.ok(recipe);
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getRecipe(@PathVariable long id) {
+        try {
+            return ResponseEntity.ok(service.getRecipe(id));
+        } catch (IllegalArgumentException ex) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<Object> postRecipe(@RequestBody RecipeDto dto) {
+        Map<String, Long> idObj = Map.of("id", service.createRecipe(dto));
+        return ResponseEntity.ok(idObj);
     }
 }
