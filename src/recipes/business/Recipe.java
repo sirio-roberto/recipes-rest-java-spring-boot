48df3392728d01571931d9f572ef77389b6ad2e8
Sirio <sirioroberto1@hotmail.com>
2023-09-17 19:43:56 -0300

    Transformed ingredients and directions into arrays and created RecipeDto to hide the id of the recipe

--- src/recipes/business/Recipe.java
+++ src/recipes/business/Recipe.java
@@ -8,8 +8,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Recipe {
+    private static long nextId = 1L;
+    private long id;
     private String name;
     private String description;
-    private String ingredients;
-    private String directions;
+    private String[] ingredients;
+    private String[] directions;
+
+    public Recipe(String name, String description, String[] ingredients, String[] directions) {
+        this.id = getNextId();
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.directions = directions;
+    }
+
+    private static long getNextId() {
+        return nextId++;
+    }
 }
---
+++ src/recipes/business/RecipeDto.java
@@ -0,0 +1,24 @@
+package recipes.business;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecipeDto {
+    private String name;
+    private String description;
+    private String[] ingredients;
+    private String[] directions;
+
+    public static Recipe convertDtoToRecipe(RecipeDto dto) {
+        return new Recipe(
+                dto.getName(),
+                dto.getDescription(),
+                dto.getIngredients(),
+                dto.getDirections()
+        );
+    }
+}
