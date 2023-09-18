5e289c4117d0ed1f07f687c7d1f4b937c4a16594
Sirio <sirioroberto1@hotmail.com>
2023-09-18 13:57:48 -0300

    Created GlobalExceptionHandler class to handle validation exceptions

---
+++ src/recipes/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package recipes.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Validation Error");
+        response.put("message", "Invalid recipe request body!");
+        return ResponseEntity.badRequest().body(response);
+    }
+}
