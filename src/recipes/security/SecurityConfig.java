95f165759e158fd3cd807c332676068ecfd27e4e
Sirio <sirioroberto1@hotmail.com>
2023-09-19 08:04:43 -0300

    Made authorization required for all recipe endpoints. Users can only shutdown and register without authorization

--- src/recipes/security/SecurityConfig.java
+++ src/recipes/security/SecurityConfig.java
@@ -2,12 +2,17 @@ package recipes.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -30,5 +35,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(matcherRegistry -> matcherRegistry
+                        .antMatchers("/api/recipe/*").authenticated()
+                        .antMatchers(HttpMethod.POST, "/actuator/shutdown").permitAll()
+                        .antMatchers(HttpMethod.POST, "/api/register").permitAll()
+                        .anyRequest().denyAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
     }
 }
